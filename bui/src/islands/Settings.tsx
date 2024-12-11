f8baf83d92da14b94612de81c48b06ecbb5b667f
Charlie Garrison <charlie-github@garrison.com.au>
2024-12-12 00:26:51 +1100

    Added a 'relative' class to the Settings island container to enable positioning of child elements
    Applied changes from BB to 1 file
    
    Files modified:
    - bui/src/islands/Settings.tsx

--- bui/src/islands/Settings.tsx
+++ bui/src/islands/Settings.tsx
@@ -25,7 +25,7 @@ export default function Settings(): JSX.Element {
 				{/* Settings Grid */}
 				<div class='grid grid-cols-1 gap-6 sm:grid-cols-2'>
 					{/* Appearance */}
-					<div class='bg-white rounded-lg shadow-sm p-6 hover:shadow-md transition-shadow'>
+					<div class='bg-white rounded-lg shadow-sm p-6 hover:shadow-md transition-shadow relative'>
 						<div class='flex items-center space-x-3'>
 							<div class='flex-shrink-0'>
 								<svg
