b946d98b6b25e5a23f54fd5ed693894f18777f67
Charlie Garrison <charlie-github@garrison.com.au>
2024-12-16 11:57:30 +1100

    more csp tuning

--- bui/src/routes/_middleware.ts
+++ bui/src/routes/_middleware.ts
@@ -19,7 +19,7 @@ export async function handler(req: Request, ctx: FreshContext) {
 	);
 	headers.set(
 		'Content-Security-Policy',
-		`default-src 'self' data: blob: https: wss:; connect-src 'self' ws://localhost http://localhost data: blob: https: wss:`,
+		`default-src 'self' 'unsafe-inline' 'unsafe-eval' data: blob: https: wss:; connect-src 'self' ws://localhost http://localhost data: blob: https: wss:`,
 	);
 	// `default-src * 'unsafe-inline' 'unsafe-eval' data: blob:; connect-src * ws: wss: http: https:`,
 
