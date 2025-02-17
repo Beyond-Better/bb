b3a00d4b719b6b9840ba4e16cac8df9bf4de0c1a
Charlie Garrison <charlie-github@garrison.com.au>
2025-02-17 14:01:24 +1100

    Added HANDLE import to main.rs
    Applied changes from BB to 1 file
    
    Files modified:
    - dui/src-tauri/src/main.rs

--- dui/src-tauri/src/main.rs
+++ dui/src-tauri/src/main.rs
@@ -1,6 +1,8 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
+use windows_sys::Win32::Foundation::HANDLE;
+
 use beyond_better_lib::{
     run
 };
