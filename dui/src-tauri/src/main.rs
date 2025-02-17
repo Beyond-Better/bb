3af10250f3093aea2e5683bb08edf34074246751
Charlie Garrison <charlie-github@garrison.com.au>
2025-02-17 14:00:50 +1100

    Improved log message formatting to display key path as a string
    Applied changes from BB to 1 file
    
    Files modified:
    - dui/src-tauri/src/main.rs

--- dui/src-tauri/src/main.rs
+++ dui/src-tauri/src/main.rs
@@ -91,7 +91,8 @@ fn main() {
                     &mut h_key
                 );
                 
-                log_content.push_str(&format!("  {} : {}\n", key_path, 
+                log_content.push_str(&format!("  {} : {}\n", key_path.iter().take(key_path.len() - 1)
+                    .map(|&c| c as u8 as char).collect::<String>(),
                     if result == 0 { "Found" } else { "Not Found" }));
                 
                 if h_key != 0 {
